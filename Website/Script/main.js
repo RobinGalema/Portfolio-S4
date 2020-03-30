46eb351f6c3636fc9ea1696e7dfe2f886633d1b1
Robin Galema <RGalwma99@gmail.com>
2020-03-30 17:47:09 +0200

    Working on sidebar menu

--- Website/Script/main.js
+++ Website/Script/main.js
@@ -3,59 +3,47 @@ let scrollText;
 let burgerMenuToggled = false;
 let brugerMenu;
 
-window.onload = function()
-{
+window.onload = function () {
     console.log("hello world");
     scrollText = document.getElementById("scrollText");
     brugerMenu = document.getElementById("burgerContainer");
     document.addEventListener("keydown", keyDown)
 }
 
-window.onscroll = () =>
-{
+window.onscroll = () => {
     console.log(canScroll);
-    if (pageYOffset >= 50)
-    {
-        if (canScroll)
-        {
-        scrollToContent("pageContainer");
-        scrollText.style.display = "none";
-        canScroll = false
+    if (pageYOffset >= 50) {
+        if (canScroll) {
+            scrollToContent("pageContainer");
+            scrollText.style.display = "none";
+            canScroll = false
         }
-    }
-    else if (pageYOffset < 100)
-    {
+    } else if (pageYOffset < 100) {
         canScroll = true;
         scrollText.style.display = "block";
     }
 }
 
-const scrollToContent = (divName) =>
-{
+const scrollToContent = (divName) => {
     document.getElementById(divName).scrollIntoView({
         behavior: 'smooth'
-      });
+    });
 }
 
-const toggleBurgerMenu = () =>
-{
-    if (burgerMenuToggled == false)
-    {
+const toggleBurgerMenu = () => {
+    if (burgerMenuToggled == false) {
         brugerMenu.style.display = "block";
         burgerMenuToggled = true;
+    } else if (burgerMenuToggled == true) {
+        brugerMenu.style.display = "none";
+        burgerMenuToggled = false;
     }
 }
 
-const keyDown = (e) =>
-{
-    if(e.code == "Escape")
-    {
-        if(burgerMenuToggled)
-        {
-            brugerMenu.style.display = "none";
-            burgerMenuToggled = false;
+const keyDown = (e) => {
+    if (e.code == "Escape") {
+        if (burgerMenuToggled) {
+            toggleBurgerMenu();
         }
     }
 }
-
-
