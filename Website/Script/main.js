490b5c308e226c2fc557ff190b0e7fac8ec48cc2
Robin Galema <RGalwma99@gmail.com>
2020-04-01 17:36:07 +0200

    SASS functionality, JS Code comments & page content
    A
    Added variables and functions in SASS.
    Added function comments in JS.
    Added page layout for SCO, DEV and PT.

--- Website/Script/main.js
+++ Website/Script/main.js
@@ -3,6 +3,7 @@ let scrollText;
 let burgerMenuToggled = false;
 let brugerMenu;
 
+// Event for when the page is loaded
 window.onload = function () {
     console.log("hello world");
     scrollText = document.getElementById("scrollText");
@@ -10,6 +11,7 @@ window.onload = function () {
     document.addEventListener("keydown", keyDown)
 }
 
+// Event for scrolling
 window.onscroll = () => {
     console.log(canScroll);
     if (pageYOffset >= 50) {
@@ -24,12 +26,20 @@ window.onscroll = () => {
     }
 }
 
+/**
+ * A function that smoothly scrolls to a given div on the page
+ * 
+ * @param {string} divName The classname of the div to scroll to.
+ */
 const scrollToContent = (divName) => {
     document.getElementById(divName).scrollIntoView({
         behavior: 'smooth'
     });
 }
 
+/**
+ * A function that toggles the side-menu on or off when clicking on the burger menu icon
+ */
 const toggleBurgerMenu = () => {
     if (burgerMenuToggled == false) {
         brugerMenu.style.display = "block";
@@ -40,7 +50,13 @@ const toggleBurgerMenu = () => {
     }
 }
 
+/**
+ * Callback function for the keydown event. This function checks which key was pressed and runs functions depending on which key was pressed.
+ * 
+ * @param {Object} e The object returned by the "keydown" event
+ */
 const keyDown = (e) => {
+    console.log(e);
     if (e.code == "Escape") {
         if (burgerMenuToggled) {
             toggleBurgerMenu();
@@ -48,6 +64,9 @@ const keyDown = (e) => {
     }
 }
 
+/**
+ * A function that sets the current page to the homepage of the website and closes any menu items that are overlaying the screen
+ */
 const homeButtonClicked = () =>
 {
     document.location.href = "/index.html#landingContainer";
@@ -59,6 +78,9 @@ const homeButtonClicked = () =>
     }
 }
 
+/**
+ * A function that sets the current page to the courses section of the home page of the website and closes any menu items that are overlaying the screen
+ */
 const coursesButtonClicked = () =>
 {
     document.location.href = "/index.html#pageContainer";
@@ -68,12 +90,23 @@ const coursesButtonClicked = () =>
     }
 }
 
+/**
+ * A function that will set the current page to the page of the desired course
+ * 
+ * @param {string} course The name of the course of which the page should be loaded
+ */
 const goToCourse = (course) =>
 {
     console.log(`/${course}.html`)
     document.location.href = `/${course}.html`;
 }
 
+/**
+ * A function that toggles content under a dropdown menu on and off
+ * 
+ * @param {Object} clickedNode The object of the node that was clicked, this is sent by adding .this as the first argument when using this function as an "onclick" in html.
+ * @param {number} index The index of the node in the list of dropdown items
+ */
 const showContent = (clickedNode, index) =>
 {
     let parentNode = clickedNode.parentNode;
